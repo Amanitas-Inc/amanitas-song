ee6cc659c9c03fafdc905f2366bafc3640173950
ropoko <rodrigostramantinoli@gmail.com>
2021-09-29 00:10:30 -0300

    feat: now the bot undestand the command play

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-// Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
 const env = require('dotenv').config()
 
-// Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS], retryLimit: 1, restRequestTimeout: 15000 });
+const prefix = '.';
 
-// When the client is ready, run this code (only once)
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ], retryLimit: 1, restRequestTimeout: 15000 });
+
 client.once('ready', () => {
     console.log('Ready!');
 });
 
-// Login to Discord with your client's token
-client.login(process.env.BOT_TOKEN).catch((e)=>console.log(e));
+client.on('messageCreate', msg => {
+    if (msg.author.bot) return;
+
+    if (!msg.content.startsWith(prefix)) return;
+
+    if (msg.content.startsWith(prefix + 'p')) {
+        msg.channel.send('você digitou: ');
+
+        const args = msg.content.trim().split(' ');
+        args.splice(0, 1);
+        msg.channel.send(args.join(' '));
+    }
+});
+
+client.login(process.env.BOT_TOKEN).catch((e) => console.log(e));
