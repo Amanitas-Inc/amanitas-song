959b7054362de05ac056d2e52a4371e93e0a8c19
ropoko <rodrigostramantinoli@gmail.com>
2021-09-30 23:31:49 -0300

    feat: now the bot can enter in a voice channel

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, VoiceChannel } = require('discord.js');
+const { joinVoiceChannel, entersState, VoiceConnectionStatus } = require('@discordjs/voice');
+// const { getVoiceConnection } = require('@discordjs/voice');
 const env = require('dotenv').config()
 
 const prefix = '.';
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ], retryLimit: 1, restRequestTimeout: 15000 });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES ], retryLimit: 1, restRequestTimeout: 15000 });
 
 client.once('ready', () => {
     console.log('Ready!');
@@ -20,6 +22,24 @@ client.on('messageCreate', msg => {
         const args = msg.content.trim().split(' ');
         args.splice(0, 1);
         msg.channel.send(args.join(' '));
+
+        const { channel } = msg.member.voice;
+
+        if (!channel) {
+            console.log("voce deve estar em chat de voz");
+            return;
+        }
+
+        const connection = joinVoiceChannel({
+            channelId: channel.id,
+            guildId: channel.guild.id,
+            adapterCreator: channel.guild.voiceAdapterCreator,
+        });
+
+        entersState(connection, VoiceConnectionStatus.Ready, 30e3);
+
+        // voice.channel.join();
+
     }
 });
 
