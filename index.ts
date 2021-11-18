ba395cf7a5b6464f2e7cb8eb4389b3e6e4004caf
ropoko <rodrigostramantinoli@gmail.com>
2021-11-18 00:16:22 -0300

    refactor: rewriting from scratch

--- index.ts
+++ index.ts
@@ -1,57 +1,61 @@
-import { Client, Intents } from 'discord.js';
-import { Player } from 'discord-player';
-import env from 'dotenv';
+import {
+  joinVoiceChannel,
+  entersState,
+  VoiceConnectionStatus,
+  createAudioPlayer,
+  NoSubscriberBehavior,
+  createAudioResource
+} from '@discordjs/voice'
+import { Client, Intents } from 'discord.js'
+import ytdl from 'ytdl-core'
+import env from 'dotenv'
 
 env.config()
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES ], retryLimit: 1, restRequestTimeout: 15000 });
 
-const player = new Player(client, { ytdlOptions: { filter: 'audioonly' } });
+const player = createAudioPlayer({
+  behaviors: {
+      noSubscriber: NoSubscriberBehavior.Pause,
+  },
+});
 
 client.once('ready', () => {
   console.log('Ready!');
 });
 
-client.on('messageCreate', async msg => {
+client.on('messageCreate', async (msg: any) => {
   if (msg.author.bot) return;
 
   if (!msg.content.startsWith(process.env.PREFIX!)) return;
 
   if (msg.content.startsWith(process.env.PREFIX! + 'p')) {
-    const args: string[] = msg.content.trim().split(' ');
-    args.splice(0, 1); // remove prefix
+      const args: string[] = msg.content.trim().split(' ');
+      args.splice(0, 1); // remove prefix
 
-    const { channel } = msg.member!.voice;
+      const { channel } = msg.member!.voice;
 
-    if (!channel) {
-      msg.channel.send("Você deve estar em um canal de voz!");
-      return;
-    }
+      if (!channel) {
+          msg.channel.send("Você deve estar em um canal de voz!");
+          return;
+      }
 
-    if (args.join(' ') === '') return;
+      const track = args.join(' ');
 
-    const track = await player.search(args.join(' '), { requestedBy: msg.member!.user }).then(x => x.tracks[0]);
+      const connection = joinVoiceChannel({
+          channelId: channel.id,
+          guildId: channel.guild.id,
+          adapterCreator: channel.guild.voiceAdapterCreator,
+      });
 
-    if (track === undefined) {
-      msg.channel.send('Não consegui encontrar o som, tente novamente!');
-      return;
-    }
+      entersState(connection, VoiceConnectionStatus.Ready, 30e3);
 
-    const queue = player.createQueue(msg.guild!, { metadata: msg.channel });
+      const stream = ytdl(track, { filter : 'audioonly', quality: 'lowestaudio' });
+      const resource = createAudioResource(stream);
 
-    try {
-      if (!queue.connection) await queue.connect(msg.member?.voice.channel!);
-    } catch {
-      console.log('Não consigo conectar!');
-      return;
-    }
-
-    queue.addTrack(track);
-
-    msg.channel.send(`A música ${track.title} foi adicionada à fila`)
-
-    if (!queue.playing) await queue.play();
+      player.play(resource);
+      connection.subscribe(player);
   }
 });
 
-client.login(process.env.BOT_TOKEN).catch((e) => console.log(e));
+client.login(process.env.BOT_TOKEN).catch((e: string) => console.log(e));
